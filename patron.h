98619428b903d70d14f732f219ed4952702a6ad3
busola18 <oluwabusolami2006@gmail.com>
2025-01-15 14:27:16 +0100

    my code

---
+++ patron.h
@@ -0,0 +1,26 @@
+#ifdef PATRON_H
+#define PATRON_H
+#include <string>
+
+class Patron{
+    private:
+    std::string name;
+    std::string libraryCardNumber;
+
+    public:
+    //Constructor
+    Patron(const std::string& name,const std::string& libraryCardNumber){};
+
+    //Getters
+    void getname()const{};
+    void getlibraryCardNumber()const{};
+
+    //Setters
+    void setname(std::string& name){};
+    void setlibraryCardNumber(int& libraryCardNumber){};
+
+    void updateContactInfo(const std::string& newContactInfo){};
+    void canBorrowMoreBooks(int currentBorrowedCount, int maxLimit){};
+
+}
+#endif //PATRON_H

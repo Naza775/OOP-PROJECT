78a657462d946a9b64cf67801105a34816e8ae7f
Adedayo06 <aminatadenrele13@gmail.com>
2025-01-15 14:34:00 -0800

    my first commit

---
+++ library_item.h
@@ -0,0 +1,33 @@
+#ifndef LIBRARY_ITEM_H
+#define LIBRARY_ITEM_H
+#include <string>
+#include<iostream>
+
+class LibraryItem{
+private:
+    std::string title;
+    bool isCheckedOut;
+    std::string dueDate;
+public:
+    LibraryItem();
+    LibraryItem(const std::string& Title, const std::string& dueDate, bool isCheckedOut);
+
+    ~LibraryItem();
+
+    void inputItem();
+    void displayItem() const;
+
+    void setTitle(const std::string& title);
+    std::string getTitle() const;
+
+    void setIsCheckedOut(bool isCheckedOut );
+    bool getIsCheckedOut() ;
+
+    void setDueDate( const std::string& dueDate);
+    std::string getDueDate() const;
+
+   
+
+    
+};
+#endif
